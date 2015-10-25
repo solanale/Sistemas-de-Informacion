a532e34145f2a97e7dab329840754e3f188ea395
naxsel <asolanas93@gmail.com>
2015-10-25 19:20:54 +0100

    Server working v0.1

--- app.js
+++ app.js
@@ -4,10 +4,9 @@
 //BASE SETUP
 
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var port = process.env.PORT || 8080;
 
 var app = express();
 
@@ -16,7 +15,7 @@ var app = express();
 var db_link = 'mongodb://localhost:27017'
 var db = mongoose.createConnection(db_link,function(error,res){
   if(error)
-    throw err;
+    throw error;
   console.log('Connected to Database');
 })
 
@@ -24,8 +23,8 @@ var db = mongoose.createConnection(db_link,function(error,res){
 // Models (tablas)
 
 var products_schema = new mongoose.Schema({
-  ID:             { type Number, unique: true },//Clave unica
-  nombre:         { type String },
+  ID:             { type: Number, unique: true }, //Clave unica
+  nombre:         { type: String },
 });
 
 var users_schema = new mongoose.Schema({
@@ -33,7 +32,7 @@ var users_schema = new mongoose.Schema({
 //  name
 //  apellido
 //  email
-  carrito:        { type Number,repeat: true }, //Array de IDs
+  carrito:        { type: Number,repeat: true }, //Array de IDs
 });
 
 //Cargar Modelos
@@ -44,8 +43,6 @@ var Users = db.model('Users', users_schema);
 // Express configuration (configuracion de la instacion)
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade'); //No usaremos jade
 
 // uncomment after placing your favicon in /public
@@ -53,7 +50,6 @@ app.set('views', path.join(__dirname, 'views'));
 //app.use(logger('dev')); //Crea un registro para dev, no necesario
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public'))); //No es necesario
 app.use(express.static(__dirname + '/public')); //Si no encuentra algo lo buscas en public
 
@@ -95,5 +91,5 @@ app.use(function(err, req, res, next) {
 
 var server = require('./routes/server')(app); //para pasar la instancia al server
 
-app.listen(config.port)
-console.log("Mogic happens on port: "+ config.port);
+app.listen(port);
+console.log("Magic happens on port: "+port);
