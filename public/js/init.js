7621398a46e9ad742331edcaf42d894fa293fb7c
Naxsel <asolanas93@gmail.com>
2015-11-21 16:59:40 +0100

    init fixed

--- public/js/init.js
+++ public/js/init.js
@@ -11,36 +11,36 @@
 		reset: 'full',
 		breakpoints: {
 			global: {
-				href: 'stylesheets/style.css',
+				href: 'css/style.css',
 				containers: 1400,
 				grid: { gutters: ['2em', 0] }
 			},
 			xlarge: {
 				media: '(max-width: 1680px)',
-				href: 'stylesheets/style-xlarge.css',
+				href: 'css/style-xlarge.css',
 				containers: 1200
 			},
 			large: {
 				media: '(max-width: 1280px)',
-				href: 'stylesheets/style-large.css',
+				href: 'css/style-large.css',
 				containers: 960,
 				grid: { gutters: ['1.5em', 0] },
 				viewport: { scalable: false }
 			},
 			medium: {
 				media: '(max-width: 980px)',
-				href: 'stylesheets/style-medium.css',
+				href: 'css/style-medium.css',
 				containers: '90%!'
 			},
 			small: {
 				media: '(max-width: 736px)',
-				href: 'stylesheets/style-small.css',
+				href: 'css/style-small.css',
 				containers: '90%!',
 				grid: { gutters: ['1.25em', 0] }
 			},
 			xsmall: {
 				media: '(max-width: 480px)',
-				href: 'stylesheets/style-xsmall.css'
+				href: 'css/style-xsmall.css'
 			}
 		},
 		plugins: {
