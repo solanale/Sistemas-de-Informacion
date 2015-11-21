48af3e08d8026d045d52e5e84f65e3274195e2fe
Naxsel <asolanas93@gmail.com>
2015-11-21 20:09:17 +0100

    el super push 2

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService',
-                             'SignUpCtrl','SignUpService','LogInCtrl','LogInService','ElementsCtrl','ElementsService']);
+//angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService',
+//                             'SignUpCtrl','SignUpService','LogInCtrl','LogInService','ElementsCtrl','ElementsService']);
 
-//angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
+angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
