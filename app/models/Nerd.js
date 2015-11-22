7f7f38a809102efd328d058668e9568a2a1029ed
Naxsel <asolanas93@gmail.com>
2015-11-22 17:18:49 +0100

    mongoose model

--- app/models/Nerd.js
+++ app/models/Nerd.js
@@ -3,9 +3,9 @@ var mongoose = require('mongoose');
 
 // define our nerd model
 // module.exports allows us to pass this to other files when it is called
-//module.exports = mongoose.model('Nerd', {
-//	name : {type : String, default: ''}
-//});
-module.exports = mongoose.model('Phones', {
-
-})
+module.exports = mongoose.model('Nerd', {
+	name : {type : String, default: ''}
+});
+//module.exports = mongoose.model('Phones', {
+//
+//})
