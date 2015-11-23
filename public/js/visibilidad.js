14aa78f5a5ee96005633d62f97bd2fe3d734d0fa
AdrianAlcolea <adrianalcoleamoreno@gmail.com>
2015-11-23 23:09:35 +0100

    ver mas y ver menos home

--- public/js/visibilidad.js
+++ public/js/visibilidad.js
@@ -3,14 +3,41 @@ function ocultarId(id)
  document.getElementById(id).style.display = 'none';
 }
 
+function ocultarClass(c)
+{
+	var cl = document.getElementsByClassName(c);
+
+	for (i in cl) {
+		cl[i].style.display = 'none';
+	}
+}
+
 function mostrarBlockId(id)
 {
  document.getElementById(id).style.display = 'block';
 }
 
+function mostrarBlockClass(c)
+{
+	var cl = document.getElementsByClassName(c);
+
+	for (i in cl) {
+		cl[i].style.display = 'block';
+	}
+}
+
 function mostrarInlineId(id)
 {
  document.getElementById(id).style.display = 'inline';
+}
+
+function mostrarInlineClass(c)
+{
+	var cl = document.getElementsByClassName(c);
+
+	for (i in cl) {
+		cl[i].style.display = 'inline';
+	}
 }
 
 function visibilidadBlockId(id)
@@ -50,5 +77,21 @@ function visibilidadInlineId(id)
 	else
 	{
 		document.getElementById(id).style.display = 'inline';
+	}
+}
+
+function visibilidadInlineClass(c)
+{
+	var cl = document.getElementsByClassName(c);
+
+	for (i in cl) {
+		if(cl[i].style.display == 'inline')
+		{
+			cl[i].style.display = 'none';
+		}
+		else
+		{
+			cl[i].style.display = 'inline';
+		}
 	}
 }
